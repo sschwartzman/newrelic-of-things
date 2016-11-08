4b13faf00a7c403c1f40d7ec7e80312e1f0fef63
Seth Schwartzman <sschwartzman@users.noreply.github.com>
2016-11-08 15:05:26 -0500

    Added HTTP get example

--- newrelic_pizero.js
+++ newrelic_pizero.js
@@ -1,10 +1,14 @@
+'use strict';
+
 var Insights = require('node-insights');
 var ds18b20 = require('ds18b20');
+var request = require('request');
 
-var insights = new Insights({
-  insertKey: 'REDACTED!',
-  accountId: 'REDACTED!'
-});
+var insightsSettings = {
+  insertKey: 'REDACTED',
+  queryKey: 'REDACTED',
+  accountId: 'REDACTED'
+}
 
 var polling_interval = 10000;
 var taking_temp = false;
@@ -12,9 +16,19 @@ var debug_on = false;
 
 function debugLog(msg) {
   if(debug_on) {
-    console.log(msg);  
+    console.log(msg);
   }
 }
+
+var insights = new Insights(insightsSettings);
+var weatherInsightsQuery = 'SELECT%20latest(%60custom.temperature%60),%20latest(%60custom.humidity%60),%20latest(%60custom.summary%60)%20FROM%20SyntheticCheck%20where%20%60custom.temperature%60%20IS%20NOT%20NULL';
+var weatherQueryOptions = {
+  url: 'https://insights-api.newrelic.com/v1/accounts/' + insightsSettings['accountId'] + '/query?nrql=' + weatherInsightsQuery,
+  headers: {
+    'Accept': 'application/json',
+    'X-Query-Key': insightsSettings['queryKey']
+  }
+};
 
 console.log('New Relic Pi Zero Thingy reporting for duty, sir/madam.');
 debugLog('Debug logging is enabled.');
@@ -35,10 +49,27 @@ setInterval(function() {
             debugLog("Current temperature is " + value + " Degrees Celsius");
             var fvalue = value * 9 / 5 + 32;
             debugLog("Current temperature is " + fvalue + " Degrees Fahrenheit");
-            insights.add({
+            var thisEvent = {
               eventType: 'PiZeroEvent',
               currTempC: value,
               currTempF: fvalue
+            };
+            request(weatherQueryOptions, function(error, response, body) {
+              if (!error && response.statusCode == 200) {
+                var data = JSON.parse(body);
+                if (data['results'] != null) {
+                  thisEvent['darksky.temperature'] = data['results'][0]['latest'];
+                  thisEvent['darksky.humidity'] = data['results'][1]['latest'];
+                  thisEvent['darksky.summary'] = data['results'][2]['latest'];
+                }
+              } else {
+                debugLog('Query to Insights failed.');
+                debugLog('Response Error: ' + error);
+                debugLog('Response Code: ' + response.statusCode);
+                debugLog('Response Body: ' + body);
+              }
+              debugLog('Sending to Insights: ' + thisEvent);
+              insights.add(thisEvent);
             });
           }
         });
