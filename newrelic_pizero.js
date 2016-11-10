559b4a6ed867b8eb2198115ac10ab8c6cee4b778
Seth Schwartzman <sschwartzman@users.noreply.github.com>
2016-11-09 22:13:28 -0500

    Big update - added REST example to js among other things

--- newrelic_pizero.js
+++ newrelic_pizero.js
@@ -1,80 +1,75 @@
 'use strict';
 
-var Insights = require('node-insights');
 var ds18b20 = require('ds18b20');
+var Insights = require('node-insights-nrot');
+var os = require('os');
 var request = require('request');
+var debug = require('debug')('nrot');
 
-var insightsSettings = {
-  insertKey: 'REDACTED',
-  queryKey: 'REDACTED',
-  accountId: 'REDACTED'
+var insights_settings = {
+    insertKey: 'REDACTED',
+    queryKey: 'REDACTED',
+    accountId: 'REDACTED'
 }
 
 var polling_interval = 10000;
-var taking_temp = false;
-var debug_on = false;
+var pizero_hostname = os.hostname() || 'PiZero-DefaultHost';
 
-function debugLog(msg) {
-  if(debug_on) {
-    console.log(msg);
-  }
-}
-
-var insights = new Insights(insightsSettings);
-var weatherInsightsQuery = 'SELECT%20latest(%60custom.temperature%60),%20latest(%60custom.humidity%60),%20latest(%60custom.summary%60)%20FROM%20SyntheticCheck%20where%20%60custom.temperature%60%20IS%20NOT%20NULL';
-var weatherQueryOptions = {
-  url: 'https://insights-api.newrelic.com/v1/accounts/' + insightsSettings['accountId'] + '/query?nrql=' + weatherInsightsQuery,
-  headers: {
-    'Accept': 'application/json',
-    'X-Query-Key': insightsSettings['queryKey']
-  }
+var insights = new Insights(insights_settings);
+var weather_insights_query = 'SELECT%20latest(%60custom.temperature%60),%20latest(%60custom.humidity%60),%20latest(%60custom.summary%60)%20FROM%20SyntheticCheck%20where%20%60custom.temperature%60%20IS%20NOT%20NULL';
+var weather_query_options = {
+    url: 'https://insights-api.newrelic.com/v1/accounts/' + insights_settings['accountId'] + '/query?nrql=' + weather_insights_query,
+    headers: {
+        'Accept': 'application/json',
+        'X-Query-Key': insights_settings['queryKey']
+    }
 };
 
 console.log('New Relic Pi Zero Thingy reporting for duty, sir/madam.');
-debugLog('Debug logging is enabled.');
+debug('Debug logging is enabled.');
 
-setInterval(function() {
-  if(!taking_temp) {
-    taking_temp = true;
-    ds18b20.sensors(function(err, ids) {
-      if (err) {
-        debugLog("Can not get sensor IDs, error: " + err);
-      }
-      debugLog('Sensor IDs', ids);
-      ids.forEach(function(id) {
-        ds18b20.temperature(id, function(err, value) {
-          if (err) {
-            debugLog("Can not get temperature for sensor ID " + id + ", error: " + err);
-          } else {
-            debugLog("Current temperature is " + value + " Degrees Celsius");
-            var fvalue = value * 9 / 5 + 32;
-            debugLog("Current temperature is " + fvalue + " Degrees Fahrenheit");
-            var thisEvent = {
-              eventType: 'PiZeroEvent',
-              currTempC: value,
-              currTempF: fvalue
+var taking_temp = false;
+
+setInterval(function () {
+    if (!taking_temp) {
+        taking_temp = true;
+        ds18b20.sensors(function (err, ids) {
+            if (err) {
+                console.log("Can not get sensor IDs: " + err);
+                taking_temp = false;
+                return;
+            }
+            debug('Sensor ID: ' + ids[0]);
+            var temp_c = ds18b20.temperatureSync(ids[0]);
+            debug("Current temperature is " + temp_c + " Degrees Celsius");
+            var temp_f = temp_c * 9 / 5 + 32;
+            debug("Current temperature is " + temp_f + " Degrees Fahrenheit");
+            var this_event = {
+                eventType: 'PiZeroEvent',
+                currTempC: temp_c,
+                currTempF: temp_f,
+                hostname: pizero_hostname
             };
-            request(weatherQueryOptions, function(error, response, body) {
-              if (!error && response.statusCode == 200) {
-                var data = JSON.parse(body);
-                if (data['results'] != null) {
-                  thisEvent['darksky.temperature'] = data['results'][0]['latest'];
-                  thisEvent['darksky.humidity'] = data['results'][1]['latest'];
-                  thisEvent['darksky.summary'] = data['results'][2]['latest'];
+            request(weather_query_options, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var data = JSON.parse(body);
+                    if (data['results'] != null) {
+                        this_event['darksky.temperature'] = data['results'][0]['latest'];
+                        this_event['darksky.humidity'] = data['results'][1]['latest'];
+                        this_event['darksky.summary'] = data['results'][2]['latest'];
+                    }
+                } else {
+                    console.log('Query to Insights failed.');
+                    console.log('Run with debug for details.');
+                    debug('Query to Insights failed.');
+                    debug('Response Error: ' + error);
+                    debug('Response Code: ' + response.statusCode);
+                    debug('Response Body: ' + body);
                 }
-              } else {
-                debugLog('Query to Insights failed.');
-                debugLog('Response Error: ' + error);
-                debugLog('Response Code: ' + response.statusCode);
-                debugLog('Response Body: ' + body);
-              }
-              debugLog('Sending to Insights: ' + thisEvent);
-              insights.add(thisEvent);
+                debug('Sending to Insights: ' + JSON.stringify(this_event));
+                insights.add(this_event);
+                taking_temp = false;
             });
-          }
         });
-      });
-    });
-    taking_temp = false;
-  }
+    }
 }, polling_interval);
--- nrotCtrl.sh
+++ nrotCtrl.sh
@@ -34,19 +34,26 @@ prereqs() {
     modprobe w1-therm
 }
 
+debugOn() {
+    export DEBUG_NROT="env DEBUG=nrot,node-insights"
+}
+
 start() {
-    
     checkpid
     STATUS=$?
-    if [ $STATUS -ne 0 ] ;
-    then
+    if [ $STATUS -ne 0 ]; then
         echo "Starting $APP_NAME..."
-        nohup $APP_PRE_OPTION $APP_PATH/$APP_FILE $APP_POST_OPTION > $APP_LOGS/$APP_FILENAME.out 2> $APP_LOGS/$APP_FILENAME.err < /dev/null &
+        if [ -n "$DEBUG_NROT" ]; then
+            echo "Debug Logging is enabled."
+            echo "Debug statements will be written to $APP_LOGS/$APP_FILENAME.err"
+            $DEBUG_NROT nohup $APP_PRE_OPTION $APP_PATH/$APP_FILE $APP_POST_OPTION > $APP_LOGS/$APP_FILENAME.out 2> $APP_LOGS/$APP_FILENAME.err < /dev/null &
+        else
+            nohup $APP_PRE_OPTION $APP_PATH/$APP_FILE $APP_POST_OPTION > $APP_LOGS/$APP_FILENAME.out 2> $APP_LOGS/$APP_FILENAME.err < /dev/null &
+        fi
         # You can un-comment next line to see what command is exactly executed
         # echo "nohup $APP_PRE_OPTION $APP_PATH/$APP_FILE $APP_POST_OPTION > $APP_LOGS/$APP_FILENAME.out 2> $APP_LOGS/$APP_FILENAME.err < /dev/null &"
         echo PID $!
         echo $! > $APP_PID
-        
         statusit
         #echo "Done"
     else
@@ -57,8 +64,7 @@ start() {
 stop() {
     checkpid
     STATUS=$?
-    if [ $STATUS -eq 0 ] ;
-    then
+    if [ $STATUS -eq 0 ]; then
         echo "Stopping $APP_NAME..."
         kill `cat $APP_PID`
         rm $APP_PID
@@ -71,27 +77,25 @@ stop() {
 
 checkpid() {
     STATUS=9
-    
-    if [ -f $APP_PID ] ;
-    then
+
+    if [ -f $APP_PID ]; then
         #echo "Is Running if you can see next line with $APP_NAME"
         ps -Fp `cat $APP_PID` | grep $APP_FILE > $TMP_FILE
-        if [ -f $TMP_FILE -a -s $TMP_FILE ] ;
-            then
+        if [ -f $TMP_FILE -a -s $TMP_FILE ]; then
                 STATUS=0
                 #"Is Running (PID `cat $APP_PID`)"
             else
                 STATUS=2
                 #"Stopped incorrectly"
             fi
-        
-        ## Clean after yourself    
+
+        ## Clean after yourself
         rm -f $TMP_FILE
     else
         STATUS=1
         #"Not Running"
     fi
-    
+
     return $STATUS
 }
 
@@ -102,23 +106,28 @@ statusit() {
     #GET return value from previous function
     STATUS=$?
     #echo $?
-    
+
     EXITSTATUS=${STATUS_CODE[STATUS]}
-    
-    if [ $STATUS -eq 0 ] ;
-    then
+
+    if [ $STATUS -eq 0 ]; then
         EXITSTATUS=${STATUS_CODE[STATUS]}" (PID `cat $APP_PID`)"
     fi
-    
+
     #echo "First Index: ${NAME[0]}"
     #echo "Second Index: ${NAME[1]}"
-    
+
     echo $APP_NAME - $EXITSTATUS
     #${STATUS_CODE[STATUS]}
-    
+
 }
 
 case "$1" in
+
+    'startDebug')
+        prereqs
+        debugOn
+        start
+        ;;
 
     'start')
         prereqs
@@ -135,12 +144,19 @@ case "$1" in
         start
         ;;
 
+    'restartDebug')
+        stop
+        prereqs
+        debugOn
+        start
+        ;;
+
     'status')
         statusit
         ;;
-        
+
     *)
-        echo "Usage: $0 { start | stop | restart | status }"
+        echo "Usage: $0 { start | startDebug | stop | restart | restartDebug | status }"
         exit 1
         ;;
 esac
